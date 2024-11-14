e53e5f12fe83920f32ee845e8af798cc88c51863
PowerSpy <120146234+PowerSpy@users.noreply.github.com>
2024-11-14 19:18:51 +0000

    FInished

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ with open("answers.txt", "r") as file:
         answers.append(line.strip())
 def fill_answers(a):
     with open("possible_answers.txt", "w") as file:
-        file.write(a)
+        file.writelines([f"{line}\n" for line in a])
 def fill_guesses(g):
     with open("good_guesses.txt", "w") as file:
-        file.write(g)
+        file.writelines([f"{line}\n" for line in g])
 def main():
     while True:
         letters = []
         let_dic = {}
+        valids = []
+        guesses = []
         user = input("Enter Locked Values (green): ")
         user_guess = input("Enter string of letters (yellow): ")
         if user == "exit":
@@ -27,9 +29,46 @@ def main():
         if len(user) == 5:
             for i, l in enumerate(user):
                 if l != "_":
-                    let_dic[(i+1)] = l
+                    let_dic[i] = l
         if user_guess != None:
             for l in user_guess:
                 letters.append(l)
         print(letters, let_dic)
+        for word in valid_words:
+            locked = True
+            unlocked = True
+            for key, val in let_dic.items():
+                if word[key] != val:
+                    locked = False
+                    break
+            if locked == False:
+                continue
+            else:
+                for l in letters:
+                    if l not in word:
+                        unlocked = False
+                        break
+            if locked and unlocked:
+                valids.append(word)
+        fill_guesses(valids)
+        for word in answers:
+            locked = True
+            unlocked = True
+            for key, val in let_dic.items():
+                if word[key] != val:
+                    locked = False
+                    break
+            if locked == False:
+                continue
+            else:
+                for l in letters:
+                    if l not in word:
+                        unlocked = False
+                        break
+            if locked and unlocked:
+                guesses.append(word)
+        fill_answers(guesses)
+                        
+
+
 main()
